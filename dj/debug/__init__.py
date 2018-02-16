8c0c4d33e30ab440fb63b0f86102a3914ad254bf
Ryan Northey <ryan@synca.io>
2018-02-16 08:59:31 +0000

    Add debug decorator

--- dj/debug/__init__.py
+++ dj/debug/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from .builtin import trace_debug
+from .decorators import debug
 from .sql import debug_sql
 from .log import log_new_queries, log_timing
 from .memory import memusage
@@ -8,5 +9,5 @@ from .timing import timings
 
 
 __all__ = (
-    "debug_sql", "log_new_queries", "log_timing", "memusage",
+    "debug", "debug_sql", "log_new_queries", "log_timing", "memusage",
     "timings", "trace_debug")
